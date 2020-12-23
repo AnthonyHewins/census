950cf5cfa6fda2571e9aaa16dd4ad5b22ed8a0ce
anthony <aahewins@protonmail.com>
2020-12-22 21:10:31 -0500

    updates

---
+++ acs.go
@@ -0,0 +1,8 @@
+package census
+
+import (
+	"fmt"
+)
+
+func ACS(year int, interval int, f Form) {
+}
--- core.go
+++ core.go
@@ -3,12 +3,89 @@ package census
 import (
 	"fmt"
 	"io/ioutil"
-	"net/url"
 	"net/http"
+	"net/url"
+	"strings"
+	"time"
 )
 
 const endpoint = "https://api.census.gov/data"
 
+// Time is a lightweight struct for specifying
+// a month and year
+type Time struct {
+	Year int
+	Month time.Month
+}
+
+func (t Time) String() string {
+	var sb strings.Builder
+
+	sb.WriteString(fmt.Sprint(t.Year))
+
+	if t.Month != 0 {
+		sb.WriteString("-%v")
+		sb.WriteString(t.Month.String())
+	}
+
+	return sb.String()
+}
+
+type Form struct {
+	// What fields to get
+	Get []string
+
+	// Predicates for the query. Slices are valid
+	Predicate map[string]interface{}
+
+	// Geography fields
+	For string
+	In string
+
+	// Time Fields
+	OnTime *Time
+	StartTime *Time
+	EndTime *Time
+}
+
+func (f Form) toValues() url.Values {
+	values := url.Values{
+		"get": {strings.Join(f.Get, ",")},
+		"in": {f.In},
+		"for": {f.For},
+	}
+
+	if f.OnTime != nil {
+		values.Add("time", f.OnTime.String())
+	} else {
+		timeQuery := []string{}
+
+		if f.StartTime != nil {
+			timeQuery = append(timeQuery, fmt.Sprintf("from+%v", f.StartTime))
+		}
+
+		if f.EndTime != nil {
+			timeQuery = append(timeQuery, fmt.Sprintf("to+%v", f.EndTime))
+		}
+
+		values.Add("time", strings.Join(timeQuery, "+"))
+	}
+
+	for field, predicate := range f.Predicate {
+		switch p := predicate.(type) {
+		case []interface{}:
+			for _, condition := p {
+				values.Add(field, fmt.Sprint(condition))
+			}
+		case interface{}:
+			values.Add(field, fmt.Sprint(p))
+		}
+	}
+}
+
+// Query is a generic function for handling generic queries that may not be handled by other
+// endpoint functions. `path` will be appended to the base URL (`api.census.gov/data`) and
+// `params` will be encoded into GET params
 func Query(path string, params url.Values) ([]byte, error) {
 	resp, err := http.Get(
 		fmt.Sprintf("%v%v?%v", endpoint, path, params.Encode()),
